ee8a8f4ba840f19c0f1c01be1e79352693e502f5
Joel Van Horn <joel@joelvanhorn.com>
2017-04-29 17:46:09 -0400

    Use `PKCS1_OAEP_PADDING` as default padding for better security (https://github.com/spikex/strongbox/issues/38)

--- lib/strongbox.rb
+++ lib/strongbox.rb
@@ -5,21 +5,21 @@ require 'strongbox/lock'
 require 'strongbox/version'
 
 module Strongbox
-  RSA_PKCS1_PADDING	= OpenSSL::PKey::RSA::PKCS1_PADDING
-  RSA_SSLV23_PADDING	= OpenSSL::PKey::RSA::SSLV23_PADDING
-  RSA_NO_PADDING		= OpenSSL::PKey::RSA::NO_PADDING
-  RSA_PKCS1_OAEP_PADDING	= OpenSSL::PKey::RSA::PKCS1_OAEP_PADDING
+  RSA_PKCS1_PADDING      = OpenSSL::PKey::RSA::PKCS1_PADDING
+  RSA_SSLV23_PADDING     = OpenSSL::PKey::RSA::SSLV23_PADDING
+  RSA_NO_PADDING         = OpenSSL::PKey::RSA::NO_PADDING
+  RSA_PKCS1_OAEP_PADDING = OpenSSL::PKey::RSA::PKCS1_OAEP_PADDING
 
   class << self
     # Provides for setting the default options for Strongbox
     def options
       @options ||= {
-        :base64 => false,
-        :symmetric => :always,
-        :padding => RSA_PKCS1_PADDING,
-        :symmetric_cipher => 'aes-256-cbc',
-        :ensure_required_columns => true,
-        :deferred_encryption => false
+        base64: false,
+        symmetric: :always,
+        padding: PKCS1_OAEP_PADDING,
+        symmetric_cipher: 'aes-256-cbc',
+        ensure_required_columns: true,
+        deferred_encryption: false
       }
     end
 
